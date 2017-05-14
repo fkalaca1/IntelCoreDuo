b19dc1d384584e4bda5676fb24369a1314facadc
tarik94 <riktash38@gmail.com>
2017-05-14 17:13:46 +0200

    Dodavanje profila

---
+++ Projekat/ParKing/ParKing/View/Profil.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ParKing.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Profil : Page
+    {
+        public Profil()
+        {
+            this.InitializeComponent();
+        }
+
+        private void profilButton_Click(object sender, RoutedEventArgs e)
+        {
+            profilParkinzi.Visibility = Visibility.Collapsed;
+            profilKontrola.Visibility = Visibility.Visible;
+        }
+
+        private void parkinziButton_Click(object sender, RoutedEventArgs e)
+        {
+            profilKontrola.Visibility = Visibility.Collapsed;
+            profilParkinzi.Visibility = Visibility.Visible;
+        }
+    }
+}
