8e0a5631336542d773db4d8e2b01667cb7181977
Aldo Selimovic <aselimovic4@etf.unsa.ba>
2017-05-21 17:06:17 +0200

    Uradjena navigacija

--- Projekat/ParKing/ParKing/ViewModel/ListaParkingaViewModel.cs
+++ Projekat/ParKing/ParKing/ViewModel/ListaParkingaViewModel.cs
@@ -7,33 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
 
 namespace ParKing.ViewModel
 {
-    public class ParkingViewModel
+    public class ListaParkingaViewModel
     {
         public ObservableCollection<Parking> Parkinzi { get; set; }
-        public Parking parking { get; set; }
 
         public ICommand Back { get; set; }
         INavigationService NavigationService { get; set; }
-        public ParkingViewModel()
+
+        public ListaParkingaViewModel()
         {
-            ObservableCollection<Parking> p=new ObservableCollection<Parking>();
+            ObservableCollection<Parking> p = new ObservableCollection<Parking>();
             Parking tmp = new Parking();
             tmp.Adresa = "Lozionicka";
-            tmp.Cijena=2.5;
+            tmp.Cijena = 2.5;
             tmp.Kapacitet = 100;
-            Image slika = new Image();
-            Uri imageUri = new Uri("ms-appx:///Assets/testpark.png");
+            
+            p.Add(tmp);
+            p.Add(tmp);
+            p.Add(tmp);
             p.Add(tmp);
             Parkinzi = p;
-            parking = tmp;
+
+
             NavigationService = new NavigationService();
             Back = new RelayCommand<object>(GoBack);
-
+       
         }
 
         public void GoBack(object parameter)
