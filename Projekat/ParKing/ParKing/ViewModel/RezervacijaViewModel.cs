a8363560ac0fbfda11dac07ccacc4f8578b71d2c
Selmir Satrovic <s.selmir@hotmail.com>
2017-05-21 22:27:41 +0200

    Dodan dijagram sekvenci , napravljene male izmjene unutar mojeRezervacije

--- Projekat/ParKing/ParKing/ViewModel/RezervacijaViewModel.cs
+++ Projekat/ParKing/ParKing/ViewModel/RezervacijaViewModel.cs
@@ -1,10 +1,12 @@
-﻿using ParKing.Model;
+﻿using ParKing.Helper;
+using ParKing.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ParKing.ViewModel
 {
@@ -12,11 +14,16 @@ namespace ParKing.ViewModel
     {
         public ObservableCollection<Rezervacija> Rezervacije { get; set; }
 
+        public ICommand Back { get; set; }
+        INavigationService NavigationService { get; set; }
+
         public RezervacijaViewModel()
         {
             ObservableCollection<Rezervacija> r = new ObservableCollection<Rezervacija>();
 
-            r.Add(new Rezervacija
+            
+
+        r.Add(new Rezervacija
             {
                 PocetakRezervacije = DateTime.ParseExact("2009-05-08,531", "yyyy-MM-dd,fff", System.Globalization.CultureInfo.InvariantCulture),
                 KrajRezervacije = DateTime.ParseExact("2019-05-08,531", "yyyy-MM-dd,fff", System.Globalization.CultureInfo.InvariantCulture),
@@ -24,7 +31,15 @@ namespace ParKing.ViewModel
             });
 
             Rezervacije = r;
+
+        NavigationService = new NavigationService();
+    Back = new RelayCommand<object>(GoBack);
         }
+
+public void GoBack(object parameter)
+{
+    NavigationService.GoBack();
+}
 
 
     }
