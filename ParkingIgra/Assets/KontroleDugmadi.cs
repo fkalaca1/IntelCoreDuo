68535d3eafd314ea87225430711c1fac41fef44b
Selmir Satrovic <s.selmir@hotmail.com>
2017-06-04 22:43:39 +0200

    Dodana igra i dijagram komponenti

---
+++ ParkingIgra/Assets/KontroleDugmadi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class KontroleDugmadi : MonoBehaviour {
+    public static bool usao = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Pokreni()
+    {
+        SceneManager.LoadScene("scena");
+        usao = false;
+
+    }
+    public static void PovecajLevel()
+    {
+        if(usao)
+        {
+            SceneManager.LoadScene("meniScena");
+            Pauza.Zavrseno = true;
+            GameManager.lvl = 0;
+            GameManager.skor = 0;
+            
+
+        }
+
+
+        if (GameManager.lvl<14)
+        {
+                GameManager.lvl++;
+               
+        }
+        else
+        {
+            usao = true;
+        }
+
+            
+        
+        if(usao==false)
+            SceneManager.LoadScene("scena");
+        //Application.LoadLevel(Application.loadedLevel);
+      
+        
+    }
+
+}
---
+++ ParkingIgra/Assets/Pauza.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Pauza : MonoBehaviour
+{
+    float levelStartDelay = 2f;
+    bool Pauzirano = false;
+    public static bool Zavrseno;
+    int skor = GameManager.skor;
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        Pauzirano = false;
+        Zavrseno = false;
+    }
+
+    
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Pauzirano)
+            {
+                SceneManager.LoadScene("meniScena");
+                Pauzirano = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Pauzirano = true;
+            }
+        }
+        else if (Input.GetKeyDown("return") && Pauzirano)
+        {
+            Time.timeScale = 1.0f;
+            Pauzirano = false;
+        }
+
+       // if (GameManager.lvl == 15)
+        //{
+          //  Zavrseno = true;
+        //}
+        skor = GameManager.skor;
+
+    }
+
+    void OnGUI()
+    {
+        var centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+
+        if (Pauzirano)
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 150, 150), "<color=black><size=30>Pauzirali ste igru</size></color>\n<size=20>Pritisnite escape za izlaz, a enter za nastavak igre</size>");
+
+        if (Zavrseno)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 250, 150), "<color=black><size=35>Cestitamo osvojili ste </size>\n" + skor + "<size=20>Pritisnite esc za izlazak</size></color>");
+            //Time.timeScale = 0.0f;
+            
+            
+
+        }
+    }
+    
+}
